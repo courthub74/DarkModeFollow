a97e0f59bf8ec98a83ed40ff617e9f6d8b8ae384
Courdevelops <courdevelops@gmail.com>
2024-09-30 16:46:41 -0400

    day nite works

---
+++ darkmode.js
@@ -0,0 +1,38 @@
+// First, we get the current theme from LocalStorage
+let darkmode = localStorage.getItem('darkmode');
+
+// Query the Theme Switch button
+const themeSwitch = document.getElementById('theme_switcher');
+
+// Function to enable Darkmode
+const enableDarkmode = () => {
+    // Add the darkmode class of the html body
+    document.body.classList.add('darkmode');
+    // Set the LocalStorage to darkmode as active 
+        // LocalStorage only stores strings
+    localStorage.setItem('darkmode', 'active');
+}
+
+// Function to disable Darkmode
+const disableDarkmode = () => {
+    // Remove the darkmode class of the html body
+    document.body.classList.remove('darkmode');
+    // Set the LocalStorage to darkmode as null
+    localStorage.setItem('darkmode', null);
+}
+
+// Enable darkmode if it's installed in LocalStorage
+// You have to tell it what to do based on what's in LocalStorage
+if(darkmode === 'active') enableDarkmode();
+
+// Add Event Listener to the theme switch button
+themeSwitch.addEventListener('click', () => {
+    // Test Button 
+    console.log("Theme Switch Pressed");
+    // Get theme set in LocalStorage
+    darkmode = localStorage.getItem('darkmode');
+    // conditional if darkmode is active or not
+        // then enable Dark or disable Dark accordingly
+            // then ABOVE define the functions to do so
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+});
